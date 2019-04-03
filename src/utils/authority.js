27521287cf5bdb7f0f7db975dc1d873c8b9bd6d1
ying-3 <shadow.yingsan@gmail.com>
2019-04-03 18:39:20 +0800

    init

---
+++ src/utils/authority.js
@@ -0,0 +1,27 @@
+// use localStorage to store the authority info, which might be sent from server in actual project.
+const { NODE_ENV } = process.env;
+
+export function getAuthority(str) {
+  // return localStorage.getItem('antd-pro-authority') || ['admin', 'user'];
+  const authorityString =
+    typeof str === 'undefined' ? localStorage.getItem('antd-pro-authority') : str;
+  // authorityString could be admin, "admin", ["admin"]
+  let authority;
+  try {
+    authority = JSON.parse(authorityString);
+  } catch (e) {
+    authority = authorityString;
+  }
+  if (typeof authority === 'string') {
+    return [authority];
+  }
+  if (!authority && NODE_ENV !== 'production') {
+    return ['admin'];
+  }
+  return authority;
+}
+
+export function setAuthority(authority) {
+  const proAuthority = typeof authority === 'string' ? [authority] : authority;
+  return localStorage.setItem('antd-pro-authority', JSON.stringify(proAuthority));
+}
